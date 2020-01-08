cd76f5d61aaae3fd6d3a0280c109a7ec4a36a8a6
Ion Gireada <ion.gireada@fintechos.com>
2020-01-08 18:42:57 +0200

    Making it work

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,20 @@
 #!/usr/bin/env node
 
+const fs = require("fs");
+const path = require("path");
+const Handlebars = require("handlebars");
+const markdown = require( "markdown" ).markdown;
+
+const program = require("commander");
+
+let text = "[Markdown] is a simple text-based [markup language]\n" +
+"created by [John Gruber]\n\n" +
+"[John Gruber]: http://daringfireball.net";
+
+// var tree = markdown.parse(text);
+
+// console.log(tree);
+
+console.log(markdown.toHTML(text));
+
+
